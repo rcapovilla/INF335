a1223c900b9a2ac42e8082df8ea656698d973111
Ricardo Capovilla <rcapovilla@gmail.com>
2022-04-27 21:23:04 -0300

    arrumado: adição de produtos no vetor; início de condição dos comandos de repetição

--- src/br/unicamp/ic/inf335/Brecho.java
+++ src/br/unicamp/ic/inf335/Brecho.java
@@ -10,16 +10,29 @@ public class Brecho {
 	private static ArrayList<ProdutoBean> produtos = new ArrayList<ProdutoBean>();
 	
 	public static void main(String[] args) {
-		ProdutoBean nProg = new ProdutoBean("CD00001","Celular Galaxy S10", "128 Gb, Preto, com Carregador",1250.0,"Poucos riscos, estado de novo.");
+		/*ProdutoBean nProg = new ProdutoBean("CD00001","Celular Galaxy S10", "128 Gb, Preto, com Carregador",1250.0,"Poucos riscos, estado de novo.");
 		nProg = new ProdutoBean("CD00002","Prod 2 ...", "Bla Bla Bla",1100.0,"Bla Bla Bla");
 		nProg = new ProdutoBean("CD00003","Prod 3 ...", "Bla Bla Bla",120.0,"Bla Bla Bla");
 		nProg = new ProdutoBean("CD00004","Prod 4 ...", "Bla Bla Bla",1300.0,"Bla Bla Bla");
 		nProg = new ProdutoBean("CD00005","Prod 5 ...", "Bla Bla Bla",9400.0,"Bla Bla Bla");
 		nProg = new ProdutoBean("CD00006","Prod 6 ...", "Bla Bla Bla",1500.0,"Bla Bla Bla");
+		produtos.add(nProg);*/
+		//condições acima não adicionariam os produtos corretamente, mas sim iria sobrescrever um sobre o outro
+		ProdutoBean nProg = new ProdutoBean("CD00001","Celular Galaxy S10", "128 Gb, Preto, com Carregador",1250.0,"Poucos riscos, estado de novo.");
+		produtos.add(nProg);
+		nProg = new ProdutoBean("CD00002","Prod 2 ...", "Bla Bla Bla",1100.0,"Bla Bla Bla");
+		produtos.add(nProg);
+		nProg = new ProdutoBean("CD00003","Prod 3 ...", "Bla Bla Bla",120.0,"Bla Bla Bla");
+		produtos.add(nProg);
+		nProg = new ProdutoBean("CD00004","Prod 4 ...", "Bla Bla Bla",1300.0,"Bla Bla Bla");
+		produtos.add(nProg);
+		nProg = new ProdutoBean("CD00005","Prod 5 ...", "Bla Bla Bla",9400.0,"Bla Bla Bla");
+		produtos.add(nProg);
+		nProg = new ProdutoBean("CD00006","Prod 6 ...", "Bla Bla Bla",1500.0,"Bla Bla Bla");
 		produtos.add(nProg);
 		
 		// Imprime produtos
-		for (int i=1; i<=produtos.size(); i++) {
+		for (int i=0; i<=produtos.size(); i++) {//condição de início errada, alterado de 1 para 0
 			System.out.println("Codigo = " + produtos.get(i).getCodigo() + " Nome = " + produtos.get(i).getNome() + " Valor = " + produtos.get(i).getValor());
 		}
 		
@@ -28,13 +41,13 @@ public class Brecho {
 		
 		System.out.println("-------------------- Produtos Ordenados -------------------");
 		// Imprime produtos ordenados
-		for (int i=1; i<=produtos.size(); i++) {
+		for (int i=0; i<=produtos.size(); i++) {//condição de início errada, alterado de 1 para 0
 			System.out.println("Codigo = " + produtos.get(i).getCodigo() + " Nome = " + produtos.get(i).getNome() + " Valor = " + produtos.get(i).getValor());
 		}
 		
 		// Calcula Média
 		Double media = 0.0;
-		int i = 1;
+		int i = 0;//condição de início errada, alterado de 1 para 0
 		while (i<=produtos.size()) {
 			media = produtos.get(i).getValor();
 			i++;
